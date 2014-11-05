bb33b8057552dfd6b9422e15d9d85ca84f430d67
Victoria Bobey <vbobey@ualberta.ca>
2014-11-05 13:34:59 -0700

    Added command line options to create_data.rb

--- README.md
+++ README.md
@@ -16,3 +16,13 @@ BOOM DONE.
 
 If you require additional Ruby gems, please add them to the Gemfile and install
 with bundle install versus running "gem install blah" manually.
+
+##### Using the create data script.
+Create data generates the number of documents specified.
+Examples:
+Create 1 document: ruby create_data.rb gen 1
+Create 5 documents: ruby create_data.rb gen 5
+
+To use a loader simply use [--loader]
+Example:
+Create 5 documents and use a loader: ruby create_data.rb gen --loader gen 5
--- create_data.rb
+++ create_data.rb
@@ -5,18 +5,29 @@ require "thor"
 require_relative "loader"
 
 class CreateData < Thor
-    desc "n", "Generate n documents"
-    def numDocs(n = 1)
-        generate(n)
-    end
+    option :loader, :type => :boolean, :desc => "Indicate if we want to use a Loader."
+    desc "gen", "Generate n documents."
+    long_desc <<-LONGDESC
+    create_date gen will generate the number of documents specified.
+ 
+    [--loader] Passes a loader to generate(). If this flag is not used, generate() will not 
+    use a loader by default.
+    LONGDESC
+
+    def gen(n)
+        # Convert number of documents to generate to an interger 
+        n = n.to_i
+
+        # Generate n documents, use a loader if called with loader flag. 
+        if options[:loader]
+            puts "Using a loader"
+            loader = Loader.new()
+            generate(n, loader)
+        else
+            puts "Not using a loader"
+            generate(n)
+        end
+    end 
 end
 
 CreateData.start(ARGV)
-
-# Testing with a value of 10
-#generate(10)
-
-loader = Loader.new()
-
-# Testing with a value of 10
-generate(10, loader)
