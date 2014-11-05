27e04845e7808a6c2a09f80bc2a84508461a6fa7
derekdowling <me@derekdowling.com>
2014-11-05 15:01:04 -0700

    Updating docs, names, making generator a class

--- cli.rb
+++ cli.rb
@@ -8,26 +8,26 @@ require_relative "generator"
 require_relative "loader"
 
 # This is a CLI tool that handles the generation and loading of data for our elastic search profiler.
-class CreateData < Thor
-    
+class CLI < Thor
+
     # Profiling variabes
     @start_time
     @end_time
-    
+
     # CLI options parameter
     option :driver, :type => :boolean, :desc => "Indicate if we want to use a driver."
     option :manifest, :type => :string, :desc => "NOT YET IMPLEMENTED, allows you to specify a specific manifest"
     option :profile, :type => :boolean, :desc => "NOT YET IMPLEMENTED, Adds profiling to the given call"
-    
+
     # CLI Commands
     desc "gen", "Generate n documents from json manifest and optionally load into ES"
     desc "query", "-- NOT YET IMPLEMENTED -- use to run elastic search queries through"
-    
+
     long_desc <<-LONGDESC
-    create_data:
+    clo:
     gen will generate the number of documents specified and optionally load them into ES
     query -- NOT YET IMPLEMENTED -- use to run elastic search queries through
-    
+
     Notes:
     [--driver] Passes our ES driver to generate(). If this flag is not set, gen will output results to STDOUT
     LONGDESC
@@ -39,33 +39,46 @@ class CreateData < Thor
     def gen(num_docs)
         # Convert number of documents to generate to an interger 
         num_docs = num_docs.to_i
+        generator = Generator.new
 
         # Generate n documents, and loads them into elastic search. 
         if options[:driver]
-            puts "Outputting results via our ES Driver"
+            puts "Loading results into ES"
             loader = Loader.new()
-            generate(num_docs, loader)
-        # otherwise just output our generated data to STDOUT
-        else
-            puts "Test mode, outputting results to STDOUT"
-            generate(num_docs)
+            generator.setDriver(loader)
+        end
+
+        if options[:profile]
+            start_profiler
+        end
+
+        # generate and output
+        generator.generate(num_docs)
+
+        if options[:profile]
+            stop_and_stat("load " << num_docs << "docs")
         end
     end
-    
+
     # Used to run queries through our ES Driver
     def query(json)
         if options[:profile]
             start_profiler
+        end
+
         # TODO: implement this with arguements   
+
         if options[:profile]
             stop_and_stat("query")
+        end
     end
-    
+
+    # captures and sets up profiling metrics
     def start_profiler
         puts "Starting at " << Time.now
         @start_time = Time.now
     end
-    
+
     # stops profiling metrics and prints to STDOUT
     # context allows you to pass in some arbitrary statistics to output, we
     # may want to look into a profiling gem that can probably do this way better
@@ -74,7 +87,7 @@ class CreateData < Thor
         # stop metrics and calculate
         @end_time = Time.now
         @elapsed = (@end_time - @start_time) * 1000.0
-        
+
         # output results
         puts "Finised at " << @end_time
         puts "Took " << @elapsed << " seconds to " << context
@@ -82,4 +95,4 @@ class CreateData < Thor
 end
 
 # Start CLI and pass in cmd line args
-CreateData.start(ARGV)
+CLI.start(ARGV)
--- generator.rb
+++ generator.rb
@@ -1,80 +1,89 @@
 require 'json'
 
-# Call this to generate json output
-# Has some default values for parameters, can pass in own values
-def generate(documents = 1, output = IO.new(STDOUT.fileno))
 
-    puts "Starting data generation"
+class Generator
 
-    # load manifest from file
-    key_values = parseManifest()
-    # start generating json objects
-    createJson(output, key_values, documents)
-end
+    # the output driver to use
+    @driver
 
-# Loads the manifest file, parses it to Json, returns the hash
-def parseManifest()
-
-    puts "Parsing manifest file"
-
-    file = File.read('manifest.json')
-    key_values = JSON.parse(file)
-    puts key_values
-
-    return key_values
-end
-
-# Create JSON objects until count is reached,
-# each time calling
-def createJson(buffer, manifest, count)
-
-    puts "Starting json output"
-
-    i = 0
-    while i < count
-
-        #puts "Generating " << i + 1 << " of " << count
-        puts "Generating #{i+1} of #{count}"
-
-        #generate data, this is a place holder below
-        json_obj = ''
-
-        #generate random inputs based on keys/values(types) provided from
-        #manifest
-        json_obj = manifest.clone
-        json_obj.each {|key, val| json_obj[key] = getRandom(val)}
-    
-        #output data to specified buffer
-        buffer.puts JSON.pretty_generate(json_obj)
-
-        i = i + 1
+    # sets up the Generator
+    def initialize(driver = IO.new(STDOUT.fileno))
+        @driver
     end
 
-    # finally close our buffer
-    puts "Finished, closing buffer"
-    buffer.close()
-    puts "Goodbye!"
-end 
+    def setDriver(driver)
+        @driver = driver
+    end
 
-# Given the type of data needed, generates random data
-# I used random number for the max vals, feel free to change
-def getRandom(value_type)
-    my_prng = Random.new(seed = Random.new_seed)
-    if value_type == 'float'
-        # Takes any float between 0 and 10,000.00...
-        # Rounds to 2 decimal places
-        return my_prng.rand(10000.0).round(2)
-    elsif value_type == 'int'
-        # Int between 0 and 100,000
-        return my_prng.rand(100000)
-    elsif value_type == 'string'
-        # String of 20 random letters
-        return ('a'..'z').to_a.shuffle[0,20].join
-    else
-        return 0
+    # Call this to generate json output
+    # Has some default values for parameters, can pass in own values
+    def generate(documents = 1)
+
+        puts "Starting data generation"
+
+        # load manifest from file
+        key_values = parseManifest()
+        # start generating json objects
+        createJson(output, key_values, documents)
+    end
+
+    # Loads the manifest file, parses it to Json, returns the hash
+    def parseManifest()
+
+        puts "Parsing manifest file"
+
+        file = File.read('manifest.json')
+        key_values = JSON.parse(file)
+        puts key_values
+
+        return key_values
+    end
+
+    # Create JSON objects until count is reached,
+    # each time calling
+    def createJson(buffer, manifest, count)
+
+        puts "Starting json output"
+
+        i = 0
+        while i < count
+
+            #puts "Generating " << i + 1 << " of " << count
+            puts "Generating #{i+1} of #{count}"
+
+            #generate random inputs based on keys/values(types) provided from
+            #manifest
+            json_obj = manifest.clone
+            json_obj.each {|key, val| json_obj[key] = getRandom(val)}
+
+            #output data to specified buffer
+            @driver.puts JSON.pretty_generate(json_obj)
+
+            i = i + 1
+        end
+
+        # finally close our buffer
+        puts "Finished, closing buffer"
+        buffer.close()
+        puts "Goodbye!"
+    end 
+
+    # Given the type of data needed, generates random data
+    # I used random number for the max vals, feel free to change
+    def getRandom(value_type)
+        my_prng = Random.new(seed = Random.new_seed)
+        if value_type == 'float'
+            # Takes any float between 0 and 10,000.00...
+            # Rounds to 2 decimal places
+            return my_prng.rand(10000.0).round(2)
+        elsif value_type == 'int'
+            # Int between 0 and 100,000
+            return my_prng.rand(100000)
+        elsif value_type == 'string'
+            # String of 20 random letters
+            return ('a'..'z').to_a.shuffle[0,20].join
+        else
+            return 0
+        end
     end
 end
-
-
-
-
