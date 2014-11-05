a982e3cd5b13e1b993601160e743db0188cc27cc
Victoria Bobey <vbobey@ualberta.ca>
2014-11-05 12:30:57 -0700

    Added Thor to create_data

--- create_data.rb
+++ create_data.rb
@@ -1,6 +1,18 @@
 #/usr/bin/env ruby
 
 require_relative "generator"
+require "thor"
+
+class CreateData < Thor
+    desc "n", "Generate n documents"
+    def numDocs(n = 1)
+        generate(n)
+    end
+end
+
+CreateData.start(ARGV)
 
 # Testing with a value of 10
-generate(10)
+#generate(10)
+
+
