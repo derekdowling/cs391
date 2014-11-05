7e3f098e9ac1e286f361c5009a9609b427edfc59
Derek Dowling <thederekdowling@gmail.com>
2014-11-05 14:41:42 -0700

    Update to create_data.rb
    Adds some more documentation, tweaks the arguments ever so slightly,  adds a profiling option and logic to support that.

--- create_data.rb
+++ create_data.rb
@@ -1,33 +1,85 @@
 #/usr/bin/env ruby
 
-require_relative "generator"
+require 'rubygems'
+require 'bundler/setup'
 require "thor"
+
+require_relative "generator"
 require_relative "loader"
 
+# This is a CLI tool that handles the generation and loading of data for our elastic search profiler.
 class CreateData < Thor
-    option :loader, :type => :boolean, :desc => "Indicate if we want to use a Loader."
-    desc "gen", "Generate n documents."
+    
+    # Profiling variabes
+    @start_time
+    @end_time
+    
+    # CLI options parameter
+    option :driver, :type => :boolean, :desc => "Indicate if we want to use a driver."
+    option :manifest, :type => :string, :desc => "NOT YET IMPLEMENTED, allows you to specify a specific manifest"
+    option :profile, :type => :boolean, :desc => "NOT YET IMPLEMENTED, Adds profiling to the given call"
+    
+    # CLI Commands
+    desc "gen", "Generate n documents from json manifest and optionally load into ES"
+    desc "query", "-- NOT YET IMPLEMENTED -- use to run elastic search queries through"
+    
     long_desc <<-LONGDESC
-    create_date gen will generate the number of documents specified.
- 
-    [--loader] Passes a loader to generate(). If this flag is not used, generate() will not 
-    use a loader by default.
+    create_data:
+    gen will generate the number of documents specified and optionally load them into ES
+    query -- NOT YET IMPLEMENTED -- use to run elastic search queries through
+    
+    Notes:
+    [--driver] Passes our ES driver to generate(). If this flag is not set, gen will output results to STDOUT
     LONGDESC
 
-    def gen(n)
+    # Our data generation/loading command
+    # TODO: convert generator to a class with a configurable driver
+    # that we can define via a setter so that we can clean up this function
+    # and add profiling more easily
+    def gen(num_docs)
         # Convert number of documents to generate to an interger 
-        n = n.to_i
+        num_docs = num_docs.to_i
 
-        # Generate n documents, use a loader if called with loader flag. 
-        if options[:loader]
-            puts "Using a loader"
+        # Generate n documents, and loads them into elastic search. 
+        if options[:driver]
+            puts "Outputting results via our ES Driver"
             loader = Loader.new()
-            generate(n, loader)
+            generate(num_docs, loader)
+        # otherwise just output our generated data to STDOUT
         else
-            puts "Not using a loader"
-            generate(n)
+            puts "Test mode, outputting results to STDOUT"
+            generate(num_docs)
         end
-    end 
+    end
+    
+    # Used to run queries through our ES Driver
+    def query(json)
+        if options[:profile]
+            start_profiler
+        # TODO: implement this with arguements   
+        if options[:profile]
+            stop_and_stat("query")
+    end
+    
+    def start_profiler
+        puts "Starting at " << Time.now
+        @start_time = Time.now
+    end
+    
+    # stops profiling metrics and prints to STDOUT
+    # context allows you to pass in some arbitrary statistics to output, we
+    # may want to look into a profiling gem that can probably do this way better
+    # out of the box
+    def stop_and_stat(context)
+        # stop metrics and calculate
+        @end_time = Time.now
+        @elapsed = (@end_time - @start_time) * 1000.0
+        
+        # output results
+        puts "Finised at " << @end_time
+        puts "Took " << @elapsed << " seconds to " << context
+    end
 end
 
+# Start CLI and pass in cmd line args
 CreateData.start(ARGV)
