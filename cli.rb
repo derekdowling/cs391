f7f6ad3dbb07f9acbaff860dc0741931da5ae6eb
derekdowling <me@derekdowling.com>
2014-11-20 19:30:33 -0700

    Adding clustering options, generator tuning, bunch of other options

--- cli.rb
+++ cli.rb
@@ -5,35 +5,30 @@ require 'bundler/setup'
 require "thor"
 
 require_relative "generator"
-require_relative "loader"
+require_relative "elastic"
 
 # This is a CLI tool that handles the generation and loading of data for our elastic search profiler.
 class CLI < Thor
 
-    # CLI Commands
-    long_desc <<-LONGDESC
-    cli:
-    gen will generate the number of documents specified and optionally load them into ES
-    query -- NOT YET IMPLEMENTED -- use to run elastic search queries through
-    ping will output the health of our ES cluster
-
-    Notes:
-    [--driver] Passes our ES driver to generate(). If this flag is not set, gen will output results to STDOUT
-    LONGDESC
-
-    desc "gen", "Generate n documents from json manifest and optionally load into ES"
-    option :driver, :type => :boolean, :desc => "Indicate if we want to use a driver."
+    desc "gen <num_docs>", "Generates <num_docs> from a JSON manifest"
+    option :driver, :type => :boolean, :aliases => :d, :desc => "Indicate if we are using the Elastic Driver"
     option :manifest, :type => :string, :desc => "NOT YET IMPLEMENTED, allows you to specify a specific manifest"
+    option :cluster, :type => :boolean, :aliases => :c, :desc => "Perform actions against the cluster if we are using the Elastic driver"
+    option :benchmark, :type => :boolean, :aliases => :b, :desc => "Benchmarks query while performing it"
     def gen(num_docs)
         # Convert number of documents to generate to an interger 
         num_docs = num_docs.to_i
         generator = Generator.new
 
         # Generate n documents, and loads them into elastic search. 
-        if options[:driver]
+        if options[:driver] || [:cluster]
             puts "Loading results into ES"
-            loader = Loader.new
-            generator.setDriver(loader)
+            elastic = Elastic.new
+
+            if options[:cluster]
+                elastic.useCluster()
+            end
+            generator.setDriver(elastic)
         end
 
         start_ts = Time.now
@@ -44,19 +39,33 @@ class CLI < Thor
         stop_and_stat(start_ts, "load " << num_docs << "docs")
     end
 
-    desc "query", "-- NOT YET IMPLEMENTED -- use to run elastic search queries through"
+    desc "query <json>", "-- NOT YET IMPLEMENTED -- use to run elastic search queries through"
+    option :cluster, :type => :boolean, :aliases => :c, :desc => "Perform actions against the cluster"
+    option :benchmark, :type => :boolean, :aliases => :b, :desc => "Benchmarks query while performing it"
     def query(json)
-        start_ts = Time.now
+        elastic = Elastic.new
+        if options[:cluster]
+            elastic.useCluster()
+        end
 
-        # TODO: implement this with arguements   
-
-        stop_and_stat(start_ts, "query")
+        if options[:benchmark]
+            elastic.query(JSON.parse(json), true)
+        else
+            elastic.query(JSON.parse(json))
+        end
     end
 
     desc "ping", "Tests our elastic search connection"
+    option :cluster, :type => :boolean, :aliases => :c, :desc => "Perform actions against the cluster"
     def ping()
-        loader = Loader.new
-        loader.testConnection()
+
+        elastic = Elastic.new
+
+        if options[:cluster]
+            elastic.useCluster()
+        end
+
+        elastic.testConnection()
     end
 
     ########################
