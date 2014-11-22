d4ab82d4ae8b8052fe6e3db6873c1c35932587ae
derekdowling <me@derekdowling.com>
2014-11-21 22:11:29 -0700

    More optimizations

--- generator.rb
+++ generator.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'rubygems'
 require 'ruby-prof'
 require 'date'
+require 'time'
 
 class Generator
 
@@ -55,7 +56,7 @@ class Generator
 
         # counters
         gen_count = 0
-        bulk_max = 450
+        bulk_max = 1500
 
         doc_size = 3000.0
         dgh = doc_size * 3600.0 / 1000000000.0
@@ -63,14 +64,8 @@ class Generator
         # tuning variables
         last_exec_ratio = 0.0
         exec_ratio = 0.0
+        # RubyProf.start
         while gen_count < count do
-
-            # tuning
-            if exec_ratio > last_exec_ratio
-                bulk_max += 20
-            else
-                bulk_max -= 20
-            end
 
             # save our last exec ratio
             last_exec_ratio = exec_ratio
@@ -83,7 +78,6 @@ class Generator
             puts "Next:#{bulk_max} - Current:#{gen_count}/#{count}"
             start_time = Time.now
 
-            # RubyProf.start
 
             while gen_count < count && bulk_count < bulk_max do
 
@@ -96,21 +90,16 @@ class Generator
 
 
                 # Add an element to the array specifying we want to index, then add the object to index.
-                obj_arr.push({ index: { _index: 'customer', _type: 'payments'} },{ data: data_hash })
+                obj_arr.push({ index: { _index: :customer, _type: :payments} },{ data: data_hash })
 
                 gen_count += 1
                 bulk_count += 1
             end
 
-            # result = RubyProf.stop
-            # printer = RubyProf::FlatPrinter.new(result)
-            # printer.print(STDOUT)
 
             # upload all the documents we generated in bulk
             if @driver.is_a?(Elastic)
                 @driver.bulk_load(obj_arr)
-            else
-                puts obj_arr
             end
 
             # Tuning Vars and Output
@@ -121,6 +110,11 @@ class Generator
             puts "#T:#{exec_time} #C:#{bulk_count+1} #R:#{exec_ratio} #G/H:#{gph}"
 
         end
+
+        # result = RubyProf.stop
+        # printer = RubyProf::FlatPrinter.new(result)
+        # printer.print(STDOUT)
+
         puts "Finished data generation!"
     end
 
@@ -143,74 +137,56 @@ class Generator
     # Given the type of data needed, generates random data
     # Generates: float, int, string, address, name, city
     def getRandom(value_type)
-        catch (:wrong_type) do
-            if value_type == 'float'
-                # Takes any float between 0 and 10,000.00...
-                # Rounds to 2 decimal places
-                return @rand.rand(10000.0).round(2)
-            elsif value_type == 'int'
-                return @rand.rand(100000)
-            elsif value_type == 'hash'
-                return @rand.rand(999999999999)
-            elsif value_type == 'small_int'
-                # Int between 0 and 30,000
-                # Want to keep this number fairly small so we
-                # have more interesting queries
-                return @rand.rand(30000)
-            elsif value_type == 'id'
-                return @rand.rand(100000)
-            elsif value_type == 'address'
-                return @rand.rand(99999)
-            elsif value_type == 'name'
-                return @name[@rand.rand(@name.length - 1)]
-            elsif value_type == 'city'
-                return @city[@rand.rand(@city.length - 1)]
-            elsif value_type == 'phone_num'
-                return @rand.rand(9999999999)
-            elsif value_type == 'email'
-                return @email[@rand.rand(@email.length - 1)]
-            elsif value_type == 'country'
-                return @country[@rand.rand(@country.length - 1)]
-            elsif value_type == 'ip'
-                return @ip[@rand.rand(@ip.length - 1)]
-            elsif value_type == 'latitude'
-                return @rand.rand(-90.0..90.0)
-            elsif value_type == 'longitude'
-                return @rand.rand(-180.0..180.0)
-            elsif value_type == 'zip'
-                return @rand.rand(99999)
-            elsif value_type == 'credit_card_num'
-                return @rand.rand(4999999999999999)
-            elsif value_type == 'credit_card_type'
-                return @cc[@rand.rand(@cc.length - 1)]
-            elsif value_type == 'credit_card_expiry_date'
-                return "#{@rand.rand(12)}" << "#{@rand.rand(31)}"
-            elsif value_type == 'company'
-                return @business[@rand.rand(@business.length - 1)]
-            elsif value_type == 'date'
-                # Random date in the past 90 days
-                return Date.today - @rand.rand(365)
-            elsif value_type == 'job_title'
-                return @job[@rand.rand(@job.length - 1)]
-            elsif value_type == 'time'
-                # Uses 24 hour time and uses format hh:mm:ss
-                hour = @rand.rand(24).to_s
-                min = @rand.rand(59)
-                if (min < 10)
-                    min = min.to_s
-                    min = "0"+min
-                end
-                sec = @rand.rand(59)
-                if (sec < 10)
-                    sec = sec.to_s
-                    sec = "0"+sec
-                end
-                return hour+":"+"#{min}"+":#{sec}"
-            else
-                puts "Ran into a problem while generating data:"
-                puts "Cannot generate random data for type '#{value_type}'."
-                throw :wrong_type
-            end
+        if value_type == 'float'
+            # Takes any float between 0 and 10,000.00...
+            # Rounds to 2 decimal places
+            return @rand.rand(10000.0).round(2)
+        elsif value_type == :int
+            return @rand.rand(100000)
+        elsif value_type == :hash
+            return @rand.rand(999999999999)
+        elsif value_type == :small_int
+            # Int between 0 and 30,000
+            # Want to keep this number fairly small so we
+            # have more interesting queries
+            return @rand.rand(30000)
+        elsif value_type == :id
+            return @rand.rand(100000)
+        elsif value_type == :address
+            return @rand.rand(99999)
+        elsif value_type == :name
+            return @name[@rand.rand(@name.length - 1)]
+        elsif value_type == :city
+            return @city[@rand.rand(@city.length - 1)]
+        elsif value_type == :phone_num
+            return @rand.rand(9999999999)
+        elsif value_type == :email
+            return @email[@rand.rand(@email.length - 1)]
+        elsif value_type == :country
+            return @country[@rand.rand(@country.length - 1)]
+        elsif value_type == :ip
+            return @ip[@rand.rand(@ip.length - 1)]
+        elsif value_type == :latitude
+            return @rand.rand(-90.0..90.0)
+        elsif value_type == :longitude
+            return @rand.rand(-180.0..180.0)
+        elsif value_type == :zip
+            return @rand.rand(99999)
+        elsif value_type == :credit_card_num
+            return @rand.rand(4999999999999999)
+        elsif value_type == :credit_card_type
+            return @cc[@rand.rand(@cc.length - 1)]
+        elsif value_type == :credit_card_expiry_date
+            return "#{@rand.rand(12)}" << "#{@rand.rand(31)}"
+        elsif value_type == :company
+            return @business[@rand.rand(@business.length - 1)]
+        elsif value_type == :date
+            # Random date in the past 90 days
+            return Date.today - @rand.rand(365)
+        elsif value_type == :job_title
+            return @job[@rand.rand(@job.length - 1)]
+        elsif value_type == :time
+            return Time.new - @rand.rand(3600)
         end
     end
 end
